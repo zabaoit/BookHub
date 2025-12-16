f6b197fba71eb3209c76851b6d09cdb9de429914
zabaoit <nguyengiabao8407@gmail.com>
2025-12-16 22:33:49 +0700

    fix(auth): wait for auth initialization in ProtectedRoute

--- frontend/src/pages/auth/ProtectedRoute .tsx
+++ frontend/src/pages/auth/ProtectedRoute .tsx
@@ -1,29 +1,23 @@
 import useAuthStore from "../../store/useAuthStore";
-import { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router";
 
 const ProtectedRoute = () => {
-  const { accessToken, loading } = useAuthStore();
-  const [starting, setStarting] = useState(true);
-
-  useEffect(() => {
-    // Simulate initialization check
-    const timer = setTimeout(() => {
-      setStarting(false);
-    }, 0);
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (starting || loading) {
+  const { accessToken, loading, isInitialized } = useAuthStore();
+  console.log("accessToken in ProtectedRoute:", accessToken);
+  // Đợi initAuth() hoàn thành trước
+  if (!isInitialized || loading) {
     return (
       <div className="flex justify-center items-center h-screen">
         <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-primary"></div>
       </div>
     );
   }
+
+  // Sau khi initialized, kiểm tra accessToken
   if (!accessToken) {
     return <Navigate to={"/signin"} replace />;
   }
+
   return <Outlet></Outlet>;
 };
 
