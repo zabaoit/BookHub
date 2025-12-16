8884839f3c45e18d884f78e160ea3f7a37581c1f
zabaoit <nguyengiabao8407@gmail.com>
2025-12-16 22:34:13 +0700

    refactor(profile): extract SideNavBarProfile component and fix user data

--- frontend/src/pages/user/AddressManagement.tsx
+++ frontend/src/pages/user/AddressManagement.tsx
@@ -1,5 +1,6 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import SideNavBarProfile from "../../components/SideNavBarProfile";
 
 const AddressManagement = () => {
   return (
@@ -8,125 +9,7 @@ const AddressManagement = () => {
       <div className="relative flex min-h-screen w-full flex-col group/design-root overflow-x-hidden">
         <div className="flex-grow w-full max-w-screen-2xl mx-auto flex">
           {/* <!-- SideNavBar --> */}
-          <aside className="w-64 flex-shrink-0 p-6 bg-background-light hidden md:block  ">
-            <div className="flex flex-col gap-8 shadow-sm p-4 bg-card-light dark:bg-card-dark rounded-lg border border-border-light dark:border-border-dark ">
-              <div className="flex items-center gap-3">
-                <div
-                  className="bg-center bg-no-repeat aspect-square bg-cover rounded-full size-12"
-                  data-alt="User profile picture"
-                  style={{
-                    backgroundImage:
-                      'url("https://lh3.googleusercontent.com/aida-public/AB6AXuCvD-0XQYPGv_MADtOUbhQKVexag3WyuWft5bWTQk7Olcj4PynoJSQwlPhsTO0nG5WIWGs2FwO4igwSr1URYMMLDSWo40R5BUiV2ExwbliSC_v0Jz-oNVWaKZ6lcL9H3tNQp6UX34Le1eQ_wA2FjVhVgFXl-u0AjAhYdQXC2Rssch1IWpgUUGXwHE3NWGMV63xVXxsBA1RchU_Ab7tuCjr60X3Wq2dLb8dQ_xR5ZGcKin0sT8E2ZQNSprk-BcGISpOX-HWw2J_A7mLv")',
-                  }}
-                ></div>
-                <div className="flex flex-col ">
-                  <h1 className="text-text-light dark:text-text-dark text-base font-bold leading-normal">
-                    Alex Doe
-                  </h1>
-                  <p className="text-text-muted-light dark:text-text-muted-dark text-sm font-normal leading-normal">
-                    alex.doe@email.com
-                  </p>
-                </div>
-              </div>
-              <nav className="flex flex-col gap-2">
-                <a
-                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
-                  href="/account/profile"
-                >
-                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
-                    person
-                  </span>
-                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
-                    Profile
-                  </p>
-                </a>
-                <a
-                  className="flex items-center gap-3 px-3 py-2 rounded-lg bg-primary/20 dark:bg-primary/30"
-                  href="/account/address-management"
-                >
-                  <span className="material-symbols-outlined fill text-primary">
-                    home_pin
-                  </span>
-                  <p className="text-primary text-sm font-bold leading-normal">
-                    Addresses
-                  </p>
-                </a>
-
-                <a
-                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
-                  href="/account/order-history"
-                >
-                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
-                    receipt_long
-                  </span>
-                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
-                    Order History
-                  </p>
-                </a>
-                {/* wishlist */}
-                {/* <a
-                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
-                  href="#"
-                >
-                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
-                    favorite
-                  </span>
-                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
-                    Wishlist
-                  </p>
-                </a> */}
-                {/* notification */}
-                {/* <a
-                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
-                  href="#"
-                >
-                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
-                    notifications
-                  </span>
-                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
-                    Notifications
-                  </p>
-                </a> */}
-                {/* chat support */}
-                {/* <a
-                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
-                  href="#"
-                >
-                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
-                    support_agent
-                  </span>
-                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
-                    Chat Support
-                  </p>
-                </a> */}
-                {/* setting */}
-                {/* <a
-                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
-                  href="#"
-                >
-                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
-                    settings
-                  </span>
-                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
-                    Settings
-                  </p>
-                </a> */}
-                <div className="border-t border-border-light dark:border-border-dark">
-                  <a
-                    className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors mt-4"
-                    href="#"
-                  >
-                    <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
-                      logout
-                    </span>
-                    <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
-                      Logout
-                    </p>
-                  </a>
-                </div>
-              </nav>
-            </div>
-          </aside>
+          <SideNavBarProfile />
 
           {/* <!-- Main Content --> */}
           <main className="flex-1 p-6 bg-background-light dark:bg-background-dark">
--- frontend/src/pages/user/ProfilePage.tsx
+++ frontend/src/pages/user/ProfilePage.tsx
@@ -1,140 +1,19 @@
-import { useNavigate } from "react-router";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import useAuthStore from "../../store/useAuthStore";
+import SideNavBarProfile from "../../components/SideNavBarProfile";
 
 const ProfilePage = () => {
-  const navigate = useNavigate();
-  const handleLogout = async () => {
-    try {
-      navigate("/signin");
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const { user } = useAuthStore();
+  console.log("User in ProfilePage:", user);
+
   return (
     <div>
       <Header />
       <div className="relative flex min-h-screen w-full flex-col group/design-root overflow-x-hidden">
         <div className="flex-grow w-full max-w-screen-2xl mx-auto flex">
           {/* <!-- SideNavBar --> */}
-          <aside className="w-64 flex-shrink-0 p-6 bg-background-light hidden md:block  ">
-            <div className="flex flex-col gap-8 shadow-sm p-4 bg-card-light dark:bg-card-dark rounded-lg border border-border-light dark:border-border-dark ">
-              <div className="flex items-center gap-3">
-                <div
-                  className="bg-center bg-no-repeat aspect-square bg-cover rounded-full size-12"
-                  data-alt="User profile picture"
-                  style={{
-                    backgroundImage:
-                      'url("https://lh3.googleusercontent.com/aida-public/AB6AXuCvD-0XQYPGv_MADtOUbhQKVexag3WyuWft5bWTQk7Olcj4PynoJSQwlPhsTO0nG5WIWGs2FwO4igwSr1URYMMLDSWo40R5BUiV2ExwbliSC_v0Jz-oNVWaKZ6lcL9H3tNQp6UX34Le1eQ_wA2FjVhVgFXl-u0AjAhYdQXC2Rssch1IWpgUUGXwHE3NWGMV63xVXxsBA1RchU_Ab7tuCjr60X3Wq2dLb8dQ_xR5ZGcKin0sT8E2ZQNSprk-BcGISpOX-HWw2J_A7mLv")',
-                  }}
-                ></div>
-                <div className="flex flex-col ">
-                  <h1 className="text-text-light dark:text-text-dark text-base font-bold leading-normal">
-                    Alex Doe
-                  </h1>
-                  <p className="text-text-muted-light dark:text-text-muted-dark text-sm font-normal leading-normal">
-                    alex.doe@email.com
-                  </p>
-                </div>
-              </div>
-              <nav className="flex flex-col gap-2">
-                <a
-                  className="flex items-center gap-3 px-3 py-2 rounded-lg bg-primary/20 dark:bg-primary/30"
-                  href="#"
-                >
-                  <span className="material-symbols-outlined fill text-primary">
-                    person
-                  </span>
-                  <p className="text-primary text-sm font-bold leading-normal">
-                    Profile
-                  </p>
-                </a>
-                <a
-                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
-                  href="address-management"
-                >
-                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
-                    home_pin
-                  </span>
-                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
-                    Addresses
-                  </p>
-                </a>
-                <a
-                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
-                  href="/account/order-history"
-                >
-                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
-                    receipt_long
-                  </span>
-                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
-                    Order History
-                  </p>
-                </a>
-                {/* wishlist */}
-                {/* <a
-                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
-                  href="#"
-                >
-                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
-                    favorite
-                  </span>
-                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
-                    Wishlist
-                  </p>
-                </a> */}
-                {/* notification */}
-                {/* <a
-                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
-                  href="#"
-                >
-                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
-                    notifications
-                  </span>
-                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
-                    Notifications
-                  </p>
-                </a> */}
-                {/* chat support */}
-                {/* <a
-                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
-                  href="#"
-                >
-                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
-                    support_agent
-                  </span>
-                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
-                    Chat Support
-                  </p>
-                </a> */}
-                {/* setting */}
-                {/* <a
-                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
-                  href="#"
-                >
-                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
-                    settings
-                  </span>
-                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
-                    Settings
-                  </p>
-                </a> */}
-                <div className="border-t border-border-light dark:border-border-dark">
-                  <a
-                    onClick={handleLogout}
-                    className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors mt-4"
-                  >
-                    <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
-                      logout
-                    </span>
-                    <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
-                      Logout
-                    </p>
-                  </a>
-                </div>
-              </nav>
-            </div>
-          </aside>
+          <SideNavBarProfile />
           {/* <!-- Main Content --> */}
           <main className="flex-1 p-6 bg-background-light dark:bg-background-dark">
             <div className="flex flex-col gap-6">
@@ -157,7 +36,7 @@ const ProfilePage = () => {
                     <input
                       className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg text-text-light dark:text-text-dark focus:outline-0 focus:ring-2 focus:ring-primary/50 border border-border-light dark:border-border-dark bg-card-light dark:bg-card-dark h-12 placeholder:text-text-muted-light dark:placeholder:text-text-muted-dark px-4 py-2 text-base font-normal leading-normal"
                       // readOnly={true}
-                      value="Alex Doe"
+                      value={user?.username}
                     />
                   </label>
                   <label className="flex flex-col">
@@ -167,7 +46,7 @@ const ProfilePage = () => {
                     <input
                       className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg text-text-light dark:text-text-dark focus:outline-0 focus:ring-2 focus:ring-primary/50 border border-border-light dark:border-border-dark bg-card-light dark:bg-card-dark h-12 placeholder:text-text-muted-light dark:placeholder:text-text-muted-dark px-4 py-2 text-base font-normal leading-normal"
                       //   readonly=""
-                      value="alex.doe@email.com"
+                      value={user?.email}
                     />
                   </label>
                   <label className="flex flex-col">
@@ -177,7 +56,7 @@ const ProfilePage = () => {
                     <input
                       className="form-input flex w-full min-w-0 flex-1 resize-none overflow-hidden rounded-lg text-text-light dark:text-text-dark focus:outline-0 focus:ring-2 focus:ring-primary/50 border border-border-light dark:border-border-dark bg-card-light dark:bg-card-dark h-12 placeholder:text-text-muted-light dark:placeholder:text-text-muted-dark px-4 py-2 text-base font-normal leading-normal"
                       //   readonly=""
-                      value="+1 (555) 123-4567"
+                      value=""
                     />
                   </label>
                   <label className="flex flex-col">
