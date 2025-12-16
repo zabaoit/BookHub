8252cc5f6921a7ded769a0129117ae12b2c5e2b6
zabaoit <nguyengiabao8407@gmail.com>
2025-12-16 22:34:37 +0700

    feat(auth): complete authentication system overhaul

---
+++ frontend/src/components/SideNavBarProfile.tsx
@@ -0,0 +1,140 @@
+import { Link, useNavigate } from "react-router";
+import useAuthStore from "../store/useAuthStore";
+
+const SideNavBarProfile = () => {
+  const { user, logout } = useAuthStore();
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/signin");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="bg-background-light">
+      <aside className="w-64 flex-shrink-0 p-6  hidden md:block  ">
+        <div className="flex flex-col gap-8 shadow-sm p-4 bg-card-light dark:bg-card-dark rounded-lg border border-border-light dark:border-border-dark ">
+          <div className="flex items-center gap-3">
+            <div
+              className="bg-center bg-no-repeat aspect-square bg-cover rounded-full size-12"
+              data-alt="User profile picture"
+              style={{
+                backgroundImage:
+                  'url("https://lh3.googleusercontent.com/aida-public/AB6AXuCvD-0XQYPGv_MADtOUbhQKVexag3WyuWft5bWTQk7Olcj4PynoJSQwlPhsTO0nG5WIWGs2FwO4igwSr1URYMMLDSWo40R5BUiV2ExwbliSC_v0Jz-oNVWaKZ6lcL9H3tNQp6UX34Le1eQ_wA2FjVhVgFXl-u0AjAhYdQXC2Rssch1IWpgUUGXwHE3NWGMV63xVXxsBA1RchU_Ab7tuCjr60X3Wq2dLb8dQ_xR5ZGcKin0sT8E2ZQNSprk-BcGISpOX-HWw2J_A7mLv")',
+              }}
+            ></div>
+            <div className="flex flex-col ">
+              <h1 className="text-text-light dark:text-text-dark text-base font-bold leading-normal">
+                {user?.username}
+              </h1>
+              <p className="text-text-muted-light dark:text-text-muted-dark text-sm font-normal leading-normal">
+                {user?.email}
+              </p>
+            </div>
+          </div>
+          <nav className="flex flex-col gap-2">
+            <Link
+              className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
+              to="/account/profile"
+            >
+              <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
+                person
+              </span>
+              <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
+                Profile
+              </p>
+            </Link>
+
+            <Link
+              className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
+              to="/account/address-management"
+            >
+              <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
+                home_pin
+              </span>
+              <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
+                Addresses
+              </p>
+            </Link>
+            <Link
+              className="flex items-center gap-3 px-3 py-2 rounded-lg bg-primary/20 dark:bg-primary/30"
+              to="/account/order-history"
+            >
+              <span className="material-symbols-outlined fill text-primary">
+                receipt_long
+              </span>
+              <p className="text-primary text-sm font-bold leading-normal">
+                Order History
+              </p>
+            </Link>
+            {/* wishlist */}
+            {/* <a
+                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
+                  href="#"
+                >
+                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
+                    favorite
+                  </span>
+                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
+                    Wishlist
+                  </p>
+                </a> */}
+            {/* notification */}
+            {/* <a
+                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
+                  href="#"
+                >
+                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
+                    notifications
+                  </span>
+                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
+                    Notifications
+                  </p>
+                </a> */}
+            {/* chat support */}
+            {/* <a
+                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
+                  href="#"
+                >
+                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
+                    support_agent
+                  </span>
+                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
+                    Chat Support
+                  </p>
+                </a> */}
+            {/* setting */}
+            {/* <a
+                  className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors"
+                  href="#"
+                >
+                  <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
+                    settings
+                  </span>
+                  <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
+                    Settings
+                  </p>
+                </a> */}
+            <div className="border-t border-border-light dark:border-border-dark">
+              <a
+                className="flex items-center gap-3 px-3 py-2 rounded-lg hover:bg-primary/10 dark:hover:bg-primary/20 transition-colors mt-4"
+                onClick={handleLogout}
+              >
+                <span className="material-symbols-outlined text-text-muted-light dark:text-text-muted-dark">
+                  logout
+                </span>
+                <p className="text-text-light dark:text-text-dark text-sm font-medium leading-normal">
+                  Logout
+                </p>
+              </a>
+            </div>
+          </nav>
+        </div>
+      </aside>
+    </div>
+  );
+};
+
+export default SideNavBarProfile;
