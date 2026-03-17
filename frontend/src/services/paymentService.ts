cac758a4f87b2e82f7a5e71509fab1603b78df0a
zabaoit <nguyengiabao8407@gmail.com>
2026-03-17 17:17:30 +0700

    fix(checkout): fix payment api route endpoint string

--- frontend/src/services/paymentService.ts
+++ frontend/src/services/paymentService.ts
@@ -2,11 +2,11 @@ import api from "../libs/axios";
 
 export const paymentService = {
   createMoMoPayment: async (data: { orderId: number; amount: number }) => {
-    const response = await api.post("/payments/momo/create", data);
+    const response = await api.post("/payment/momo/create", data);
     return response.data;
   },
   createVNPayPayment: async (data: { orderId: number; amount: number }) => {
-    const response = await api.post("/payments/vnpay/create", data);
+    const response = await api.post("/payment/vnpay/create", data);
     return response.data;
   },
 };
